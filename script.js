13d1e6d00bed615136dcafaae643fb1480fd1f71
gusrod22 <90981527+gusrod22@users.noreply.github.com>
2023-03-31 00:08:23 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -5,16 +5,22 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   
-  // Function to randomize the grid layout
-  function randomizeGridLayout() {
+// Function to randomize the grid layout
+function randomizeGridLayout() {
+    const layouts = [
+      { layout1: '1 / 1 / 3 / 2', layout2: '1 / 2 / 2 / 3', layout3: '2 / 2 / 3 / 3', layout4: '3 / 1 / 4 / 3' },
+      { layout1: '1 / 1 / 2 / 2', layout2: '2 / 1 / 3 / 2', layout3: '1 / 2 / 2 / 3', layout4: '2 / 2 / 4 / 3' },
+      { layout1: '1 / 1 / 2 / 3', layout2: '2 / 1 / 3 / 2', layout3: '3 / 1 / 4 / 2', layout4: '2 / 2 / 4 / 3' },
+    ];
+  
+    const randomLayout = layouts[getRandomInt(0, layouts.length - 1)];
     const imageContainers = document.querySelectorAll('.image-container');
-    
-    imageContainers.forEach((container) => {
-      container.style.gridRowEnd = 'span ' + getRandomInt(1, 2);
-      container.style.gridColumnEnd = 'span ' + getRandomInt(1, 2);
+  
+    imageContainers.forEach((container, index) => {
+      container.style.gridArea = randomLayout[`layout${index + 1}`];
     });
   }
-  
+    
   // Debounce function
   function debounce(func, wait) {
     let timeout;
