986a6b860e90bbc0cb8dd4a0661eae4350ed982a
gusrod22 <90981527+gusrod22@users.noreply.github.com>
2023-03-31 01:41:28 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,46 +1,78 @@
 // Function to generate a random number between min and max
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  
-  // Function to randomize the grid layout
-  function randomizeGridLayout() {
-    const layouts = [
-      ['1 / 1 / 3 / 2', '1 / 2 / 2 / 3', '2 / 2 / 3 / 3', '3 / 1 / 4 / 3'],
-      ['1 / 1 / 2 / 2', '2 / 1 / 3 / 2', '1 / 2 / 2 / 3', '2 / 2 / 4 / 3'],
-      ['1 / 1 / 2 / 3', '2 / 1 / 3 / 2', '3 / 1 / 4 / 2', '2 / 2 / 4 / 3'],
-    ];
-  
-    const randomLayout = layouts[getRandomInt(0, layouts.length - 1)];
-    const imageContainers = document.querySelectorAll('.image-container');
-  
-    imageContainers.forEach((container, index) => {
-      container.style.gridArea = randomLayout[index];
-    });
-  }
-  
-  // Debounce function
-  function debounce(func, wait) {
-    let timeout;
-    return function (...args) {
-      const context = this;
-      clearTimeout(timeout);
-      timeout = setTimeout(() => func.apply(context, args), wait);
-    };
-  }
-  
-  // Attach the randomizeGridLayout function to the wheel event of the image grid
-  const imageGrid = document.querySelector('.image-grid');
-  imageGrid.addEventListener('wheel', (event) => {
-    event.preventDefault(); // Add this line to prevent the main scrollbar from scrolling
-    randomizeGridLayout();
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Function to create a clone of the image grid
+function cloneImageGrid(imageGrid) {
+  const clone = imageGrid.cloneNode(true);
+  clone.style.opacity = 0;
+  clone.style.position = "absolute";
+  return clone;
+}
+
+// Function to randomize the grid layout in the clone
+function randomizeGridLayout(clone) {
+  const layouts = [
+    [
+      "1 / 1 / span 2 / span 2",
+      "1 / 3 / span 1 / span 1",
+      "2 / 3 / span 1 / span 1",
+      "3 / 1 / span 1 / span 3",
+    ],
+    [
+      "1 / 1 / span 1 / span 2",
+      "2 / 1 / span 1 / span 2",
+      "1 / 3 / span 1 / span 1",
+      "2 / 3 / span 2 / span 1",
+    ],
+    [
+      "1 / 1 / span 1 / span 3",
+      "2 / 1 / span 1 / span 1",
+      "3 / 1 / span 1 / span 1",
+      "2 / 2 / span 2 / span 1",
+    ],
+  ];
+
+  const randomLayout = layouts[getRandomInt(0, layouts.length - 1)];
+  const imageContainers = clone.querySelectorAll(".image-container");
+
+  imageContainers.forEach((container, index) => {
+    container.style.gridArea = randomLayout[index];
   });
-  
-  // Add smooth transition between layouts
-  const imageContainers = document.querySelectorAll('.image-container');
-  imageContainers.forEach((container) => {
-    container.style.transition = 'all 0.5s ease';
-  });
-  
+}
+
+// Function to handle swipe up gesture and smoothly switch layouts
+function handleSwipeUp(event, imageGrid) {
+  const clone = cloneImageGrid(imageGrid);
+  randomizeGridLayout(clone);
+  imageGrid.parentNode.insertBefore(clone, imageGrid);
+
+  imageGrid.style.transition = "opacity 0.25s";
+  imageGrid.style.opacity = 0;
+  clone.style.transition = "opacity 0.25s";
+  clone.style.opacity = 1;
+
+  setTimeout(() => {
+    imageGrid.parentNode.removeChild(imageGrid);
+  }, 250);
+}
+
+// Swipe gesture detection
+let touchStartY;
+const imageGrid = document.querySelector(".image-grid");
+
+imageGrid.addEventListener("touchstart", (event) => {
+  touchStartY = event.touches[0].clientY;
+});
+
+imageGrid.addEventListener("touchend", (event) => {
+  const touchEndY = event.changedTouches[0].clientY;
+  const deltaY = touchStartY - touchEndY;
+
+  if (deltaY > 50) {
+    handleSwipeUp(event, imageGrid);
+  }
+});
