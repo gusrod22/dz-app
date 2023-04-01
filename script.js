f88a6808ce5eb79665c056f3518e22b7af0084be
gusrod22 <90981527+gusrod22@users.noreply.github.com>
2023-04-01 19:02:34 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Function to generate a random number between min and max
-// This function is used later to select a random layout for the new image-grid
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -7,7 +6,6 @@ function getRandomInt(min, max) {
 }
 
 // Function to create a clone of the image grid
-// This function is used later to create a new image grid with a randomized layout
 function cloneImageGrid(imageGrid) {
   const clone = imageGrid.cloneNode(true);
   clone.style.opacity = 0;
@@ -16,37 +14,17 @@ function cloneImageGrid(imageGrid) {
 }
 
 // Function to randomize the grid layout in the clone
-// this function is used later to create a new image
-let previousLayout;
-
 function randomizeGridLayout(clone) {
   const layouts = [
     "'a b' 'a c' 'd d'",
-    "'a b' 'a d' 'c d'",
-    "'a b' 'c a' 'c d'",
     "'a a' 'b c' 'd d'",
-    "'a a' 'c b' 'd d'",
-    "'a a' 'c c' 'b d'",
-    "'a b' 'c c' 'a d'",
-    "'a b' 'c d' 'a c'",
-    "'a b' 'c d' 'b c'",
-    "'a c' 'a b' 'd d'",
-    "'a c' 'a d' 'b d'",
-    "'a c' 'b c' 'a d'",
-    "'a d' 'a b' 'c d'",
-    "'a d' 'b d' 'c c'",
-    "'a d' 'c d' 'b c'",
+    "'a b' 'a d' 'c d'",
+    "'b a' 'c a' 'c d'",
   ];
 
-  let randomLayout;
-  do {
-    randomLayout = layouts[getRandomInt(0, layouts.length - 1)];
-  } while (randomLayout === clone.style.gridTemplateAreas);
-
+  const randomLayout = layouts[getRandomInt(0, layouts.length - 1)];
   clone.style.gridTemplateAreas = randomLayout;
-  addEventListeners(clone); // Add event listeners to the new grid
 }
-
 
 function addEventListeners(imageGrid) {
   imageGrid.addEventListener('touchstart', (event) => {
@@ -57,7 +35,7 @@ function addEventListeners(imageGrid) {
     const touchEndY = event.changedTouches[0].clientY;
     const deltaY = touchStartY - touchEndY;
 
-    if (deltaY > 20) {
+    if (deltaY > 50) {
       handleSwipeUp(event, imageGrid);
     }
   });
@@ -91,7 +69,10 @@ function handleSwipeUp(event, imageGrid) {
   clone.style.transition = 'opacity 0.25s';
   clone.style.opacity = 1;
 
-  imageGrid.parentNode.removeChild(imageGrid);
+  setTimeout(() => {
+    imageGrid.parentNode.removeChild(imageGrid);
+    addEventListeners(clone); // Add event listeners to the new grid
+  }, 250);
 }
 
 let touchStartY;
